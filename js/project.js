69856678866642e00d318606dfec2a72789f357f
raiya.sous <raiya.sous@PNC.LAN>
2019-12-06 09:23:24 +0700

    modified index.html and js/project.js

--- js/project.js
+++ js/project.js
@@ -1,27 +1,56 @@
 $(document).ready(function(){
-    var url ="https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
-    $.ajax({
-        dataType : "json",
-        url : url,
-        success: function(data){
-            var result = "";
-            data.recipes.forEach(item=>{      
-                item.ingredients.forEach(element=>{
-                    result +=`
-                    <tbody>
-                        <tr>
-                            <td>${element.name}</td>
-                            <td>${element.quantity}</td>
-                            <td>${element.unit}</td>
-                            <td>
-                              <img src="${element.iconUrl}" class="img-fluid" />
-                            </td>
-                        </tr>
-                    </tbody>
-                    `;
-                })
-            })
-            $('table').append(result); 
-        }           
-    })
+    requestApi();
 })
+
+////// request api
+var requestApi = () =>{
+    $.ajax({
+        dataType: 'json',
+        url : getUrl(),
+        success: (data) => getRecipe(data),
+        error : () => getError(),
+    });
+}
+
+////// get url
+
+var getUrl = () =>{
+    url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
+    return url;
+}
+
+//// get Error
+var getError = () => console.log("Error");
+
+////// get recipe
+var getRecipe = (myData) =>{
+    myData.recipes.forEach(element =>{
+        getIngredient(element.ingredients);
+    });
+}
+
+////// get Ingredients
+var getIngredient = (ing) =>{
+    ing.forEach(item =>{
+        computeHTML(item);
+    })
+}
+
+///// compute to HTML
+
+var computeHTML = (display) =>{
+    var compute = "";
+    compute +=`
+    <tr>
+       <td><img src= "${display.iconUrl}" width= "100"></td>
+       <td>${display.name}</td>
+       <td>${display.quantity}</td>
+       <td>${display.unit[0]}</td>
+    </tr>
+    `;
+    printOut(compute);
+}
+
+printOut = (out) =>{
+    $('#ingredient').append(out);
+}
