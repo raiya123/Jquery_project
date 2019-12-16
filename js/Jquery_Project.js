54f56221544a8e79e2cdeef4a275bc1b31ee8154
raiya.sous <raiya.sous@PNC.LAN>
2019-12-16 09:16:31 +0700

    add new step

--- js/Jquery_Project.js
+++ js/Jquery_Project.js
@@ -31,16 +31,15 @@ function chhosenRechipe(rechipe){
 }
 //// get recipe ////
 function getRecipe(rechipeId){
+    $('#text-num').html('Number of persons');
     alldata.forEach(element =>{
         if(element.id == rechipeId){
            getEachRecipe(element.name,element.iconUrl);
            getIngredient(element.ingredients);
            getinstruction(element.instructions);
         }
-
     });   
 }
-
 /// get Recipe ////
 var getEachRecipe = (name,img) =>{
     ////// Name //////
@@ -48,39 +47,45 @@ var getEachRecipe = (name,img) =>{
     result +=`
     <h3>${name}</h3> 
     `;
-    $('#card').html(result);
+    $('#name').html(result);
     /////// Image //////
     var results = "";
     results +=`
-    <img src="${img}" width="100"> 
+    <img src="${img}" width="170" height="140"> 
     `;
-    $('#cards').html(results);
+    $('#img').html(results);
 }
-
  //// get Ingredient ///
 function getIngredient(ingredient){
     var result = "";
+    $('#nameIngredient').html('Ingredients');
     ingredient.forEach(el =>{
       result +=`
         <tr>
-          <td><img src="${el.iconUrl}" width="100" /></td>
+          <td><img src="${el.iconUrl}" width="40" height="35"  /></td>
           <td>${el.quantity}</td>
           <td>${el.unit[0].toLowerCase()}</td>
           <td>${el.name}</td>
         </tr>
       `;
     })
-    $('#table').html(result);
+    $('#ingredient').html(result);
 }
 
 ///// get Instruction ///
 function  getinstruction(instruction){
+    $('#introduction').html('Instructions');
+    var step = instruction.split("<step>");
     var instruct = "";
-      instruct +=`
-          ${instruction}
-      `;
+    //// loop to get data by step
+    for(let i=1; i<step.length; i++){
+        instruct +=`
+        <p class="text-info"><strong>Step${i}</strong></p>
+        ${step[i]}
+        `;
+    }
       $('#step').html(instruct);
 }
 
-
+///// counter Number ///
 
